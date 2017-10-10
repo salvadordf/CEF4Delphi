bd0de4a775c8f995b84520d2fbdd9bb3abcdc508
Salvador Díaz Fau <briskbard@briskbard.com>
2017-10-10 09:25:01 +0200

    Deleted unused files
    - Bug fix #56 : Deleted unused files.
    - Created the source\dcu directory for the Delphi 7 project .

--- demos/MDIBrowser/00-DeleteDCUs.bat
+++ demos/MDIBrowser/00-DeleteDCUs.bat
@@ -5,4 +5,12 @@ del /s /q *.bpi
 del /s /q *.hpp
 del /s /q *.exe
 del /s /q *.log
+del /s /q *.dsk
+del /s /q *.identcache
+del /s /q *.stat
+del /s /q *.local
 del /s /q *.~*
+rmdir Win32\Debug
+rmdir Win32\Release
+rmdir Win32
+rmdir __history
--- source/00-DeleteDCUs.bat
+++ source/00-DeleteDCUs.bat
@@ -1,2 +1,10 @@
 del /s /q *.dcu
+del /s /q *.dsk
+del /s /q *.identcache
+del /s /q *.stat
+del /s /q *.local
 del /s /q *.~*
+rmdir Win32\Debug
+rmdir Win32\Release
+rmdir Win32
+rmdir __history
