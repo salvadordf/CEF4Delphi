32c4ed2eb001833c71ca8aacc70f5b5d070541de
Salvador Díaz Fau <briskbard@briskbard.com>
2019-03-22 17:23:20 +0100

    Updated comments in Subprocess demo
    Added more info to the Lazarus package

--- demos/SubProcess/SubProcess.dpr
+++ demos/SubProcess/SubProcess.dpr
@@ -47,19 +47,22 @@ uses
   {$ENDIF}
   uCEFApplication;
 
-// CEF3 needs to set the LARGEADDRESSAWARE flag which allows 32-bit processes to use up to 3GB of RAM.
+// CEF3 needs to set the LARGEADDRESSAWARE flag which allows 32-bit processes
+// to use up to 3GB of RAM.
 {$SetPEFlags IMAGE_FILE_LARGE_ADDRESS_AWARE}
 
 begin
   GlobalCEFApp := TCefApplication.Create;
 
-  // The main process and the subprocess *MUST* have the same FrameworkDirPath, ResourcesDirPath,
-  // LocalesDirPath, cache, cookies and UserDataPath paths
+  // The main process and the subprocess *MUST* have the same GlobalCEFApp
+  // properties and events, specially FrameworkDirPath, ResourcesDirPath,
+  // LocalesDirPath, cache, cookies and UserDataPath paths.
 
-  // The demos are compiled into the BIN directory. Make sure SubProcess.exe and SimpleBrowser.exe are in that
-  // directory or this demo won't work.
+  // The demos are compiled into the BIN directory. Make sure SubProcess.exe
+  // and SimpleBrowser.exe are in that directory or this demo won't work.
 
-  // In case you want to use custom directories for the CEF3 binaries, cache, cookies and user data.
+  // In case you want to use custom directories for the CEF3 binaries, cache,
+  // cookies and user data.
 {
   GlobalCEFApp.FrameworkDirPath     := 'cef';
   GlobalCEFApp.ResourcesDirPath     := 'cef';
