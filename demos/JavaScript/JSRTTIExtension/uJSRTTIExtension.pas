8e084c205fac6498e4cf0f947db04ece8e4b4f17
Salvador Díaz Fau <briskbard@briskbard.com>
2019-05-16 16:56:05 +0200

    Fixed TChromium.OnClose event declaration in JSRTTIExtension demo

--- demos/JavaScript/JSRTTIExtension/uJSRTTIExtension.pas
+++ demos/JavaScript/JSRTTIExtension/uJSRTTIExtension.pas
@@ -96,7 +96,7 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
     procedure Chromium1Close(Sender: TObject; const browser: ICefBrowser;
-      out Result: Boolean);
+      var aAction : TCefCloseBrowserAction);
     procedure Chromium1BeforeClose(Sender: TObject;
       const browser: ICefBrowser);
   protected
@@ -340,10 +340,10 @@ begin
 end;
 
 procedure TJSRTTIExtensionFrm.Chromium1Close(
-  Sender: TObject; const browser: ICefBrowser; out Result: Boolean);
+  Sender: TObject; const browser: ICefBrowser; var aAction : TCefCloseBrowserAction);
 begin
   PostMessage(Handle, CEF_DESTROY, 0, 0);
-  Result := True;
+  aAction := cbaDelay;
 end;
 
 procedure TJSRTTIExtensionFrm.FormCloseQuery(
