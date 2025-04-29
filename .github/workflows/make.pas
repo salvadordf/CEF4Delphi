33d76d97b40794777d07ebae8c4a0f0ea1ab496f
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-04-29 21:21:55 +0300

    add DCPcrypt

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -212,7 +212,7 @@ begin
   try
     if ParamCount > 0 then
       case ParamStr(1) of
-        'build': BuildAll(Time, []);
+        'build': BuildAll(Time, ['DCPcrypt']);
         else
           OutLog(etDebug, ParamStr(1));
       end;
